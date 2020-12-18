b32236e52c8d75a48db782b829cdc6052d98b972
Iyana Medlock <iyanamedlock@gmail.com>
2020-12-17 22:25:00 -0600

    started over, ui code, timer coded, and questions are coded

--- assets/script.js
+++ assets/script.js
@@ -1,102 +1,54 @@
-let startBtn = document.getElementById('startBtn');
-let sec = document.getElementById('sec');
-const startScreen = document.getElementById('start');
-const hsScreen = document.getElementById('highscore');
-const q1 = document.getElementById('q1');
-const btn = document.getElementsByClassName("option");
-let status = document.getElementById('status');
+const startBtn = document.getElementById("startBtn");
+let time = document.getElementById("timer");
+let startScreen = document.getElementById("start");
+let btn = document.querySelectorAll("button");
+let pages = document.querySelectorAll("#q");
+let hs = document.getElementById("hs");
+let quiz = document.getElementById('quiz');
 
-startBtn.addEventListener('click', function(){
-    startScreen.classList.add("fade");
-    this.disabled = true;
-    q1.style.display = "block";
-    timer(80);
-})
+function game() {
+  let seconds = 80;
+  let clock = setInterval(function () {
+    seconds--;
+    time.textContent = seconds;
+    console.log(seconds);
 
-// let seconds = 60;
-// function timer(){
-    
-//     setInterval(function(){
-//         seconds--;
-//         sec.textContent = seconds;
-//         if (seconds < 10){
-//             sec.textContent = '0' + seconds; 
-//         }
-//         if(sec.textContent === '00'){
-//             hsScreen.style.display = 'block';
-//             clearInterval(this);
-//         }
-//         console.log(seconds);
-//     }, 1000);
-
-//     for (var i = 0; i < btn.length; i++){
-//         btn[i].addEventListener("click", function(){
-//             if (this.id === 'option'){
-//                 status.textContent = "WRONG";
-//                 setTimeout(function(){
-//                     status.textContent = "";
-//                 },3000)
-
-//                 if (sec.textContent >= 15){
-//                     let sub = sec.textContent - 15
-//                     console.log(sub);
-                    
-//                 }
-//                 else{
-//                     let reduce = 15 - seconds;
-//                     alert(reduce);
-//                 }
-                            
-//             }
-//         })
-//     }
-// }
-
-
-function timer(time){
-    let seconds = time
-
-    let clock = setInterval(function(){
-        seconds--;
-        sec.textContent = seconds;
-        if (seconds < 10){
-            sec.textContent = '0' + seconds;
-        }
-        if (seconds === 0){
-            hsScreen.style.display = 'block';
-            clearTimeout(clock);
-        }
-    }, 1000)
-
-    for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener('click', function (){
-            if (this.id === 'option'){
-                status.textContent = "WRONG";
-                setTimeout (function () {
-                    status.textContent = '';
-                }, 3000)
-    
-                let seconds = parseInt(sec.textContent);
-                if (seconds >= 15){
-                    let reduce = seconds - 15;
-                    clearInterval(clock);
-                    timer(reduce);
-                }
-                else{
-                    let reduce = 15 - seconds;
-                    clearInterval(clock);
-                    timer(reduce);
-                    
-                    
-                }
-            }
-            else{
-                status.textContent = "CORRECT";
-                setTimeout (function () {
-                    status.textContent = '';
-                }, 3000)
-            }
-        })
-    
+    if (seconds < 10) {
+      time.textContent = "0" + seconds;
     }
+
+    if (seconds === 0) {
+      clearInterval(clock);
+    }
+  }, 1000);
 }
+
+for (var i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function (e) {
+    if (this.id === "option") {
+      let seconds = parseInt(time.textContent);
+      if (time.textContent <= 15) {
+        let reduce = 15 - seconds;
+      } else {
+        let reduce = seconds - 15;
+      }
+    }
+    target = e.target;
+    if (target.matches("button") === true) {
+      let page = target.parentElement;
+      page.remove();
+
+      if (!quiz.childNodes){
+          alert("RAN");
+      }
+    }
+  });
+}
+
+startBtn.addEventListener("click", function () {
+  game();
+  startScreen.style.display = "none";
+  for (let i = 0; i < pages.length; i++) {
+    pages[i].style.display = "block";
+  }
+});
