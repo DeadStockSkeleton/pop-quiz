171658d173858bdabf4e58359a969d5514b41fed
Iyana Medlock <iyanamedlock@gmail.com>
2020-12-17 18:45:12 -0600

    coded time and fixed time reducing feature

--- assets/script.js
+++ assets/script.js
@@ -10,37 +10,93 @@ startBtn.addEventListener('click', function(){
     startScreen.classList.add("fade");
     this.disabled = true;
     q1.style.display = "block";
-    timer(seconds);
+    timer(80);
 })
-for (var i = 0; i < btn.length; i++){
-    btn[i].addEventListener("click", function(){
-        if (this.id === 'option'){
-            status.textContent = "WRONG";
-            setTimeout(function(){
-                status.textContent = "";
-            },3000)
-            let seconds = sec.textContent;
-            seconds = parseInt(seconds)
-            
-             seconds--;
-             sec.textContent = seconds;            
-        }
-    })
-}
-let seconds = 60;
+
+// let seconds = 60;
+// function timer(){
+    
+//     setInterval(function(){
+//         seconds--;
+//         sec.textContent = seconds;
+//         if (seconds < 10){
+//             sec.textContent = '0' + seconds; 
+//         }
+//         if(sec.textContent === '00'){
+//             hsScreen.style.display = 'block';
+//             clearInterval(this);
+//         }
+//         console.log(seconds);
+//     }, 1000);
+
+//     for (var i = 0; i < btn.length; i++){
+//         btn[i].addEventListener("click", function(){
+//             if (this.id === 'option'){
+//                 status.textContent = "WRONG";
+//                 setTimeout(function(){
+//                     status.textContent = "";
+//                 },3000)
+
+//                 if (sec.textContent >= 15){
+//                     let sub = sec.textContent - 15
+//                     console.log(sub);
+                    
+//                 }
+//                 else{
+//                     let reduce = 15 - seconds;
+//                     alert(reduce);
+//                 }
+                            
+//             }
+//         })
+//     }
+// }
+
+
 function timer(time){
-    seconds = time;
-    setInterval(function(){
+    let seconds = time
+
+    let clock = setInterval(function(){
         seconds--;
         sec.textContent = seconds;
         if (seconds < 10){
-            sec.textContent = '0' + seconds; 
+            sec.textContent = '0' + seconds;
         }
-        if(seconds === 0){
+        if (seconds === 0){
             hsScreen.style.display = 'block';
-            clearInterval();
+            clearTimeout(clock);
         }
-        console.log(seconds);
-    }, 1000);
-}
+    }, 1000)
 
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].addEventListener('click', function (){
+            if (this.id === 'option'){
+                status.textContent = "WRONG";
+                setTimeout (function () {
+                    status.textContent = '';
+                }, 3000)
+    
+                let seconds = parseInt(sec.textContent);
+                if (seconds >= 15){
+                    let reduce = seconds - 15;
+                    clearInterval(clock);
+                    timer(reduce);
+                }
+                else{
+                    let reduce = 15 - seconds;
+                    clearInterval(clock);
+                    timer(reduce);
+                    
+                    
+                }
+            }
+            else{
+                status.textContent = "CORRECT";
+                setTimeout (function () {
+                    status.textContent = '';
+                }, 3000)
+            }
+        })
+    
+    }
+}
